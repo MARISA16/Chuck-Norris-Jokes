fb5fc14375ee5f0a11ef2e3d85585c6a43dd5607
CIANDT\marisah <marisa2014antniomarisa@outlook.com>
2021-11-26 16:03:15 -0300

    final

--- app/src/main/java/com/marisa/chucknorrisjokes/ActivityChuckNorrisJokes.kt
+++ app/src/main/java/com/marisa/chucknorrisjokes/ActivityChuckNorrisJokes.kt
@@ -1,22 +1,20 @@
 package com.marisa.chucknorrisjokes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.marisa.chucknorrisjokes.databinding.ActivityChuckNorrisJokesBinding
 import com.marisa.chucknorrisjokes.models.DadosChuck
 import com.marisa.chucknorrisjokes.repositories.Repository
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
 class ActivityChuckNorrisJokes : AppCompatActivity() {
 
     private lateinit var binding: ActivityChuckNorrisJokesBinding
 
-    val repository = Repository()
+    private val repository = Repository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,26 +28,21 @@ class ActivityChuckNorrisJokes : AppCompatActivity() {
         }
     }
 
-
     private fun click() {
+        val result = repository.getChuck()
 
-        val result =repository.getChuck()
-
-        result.enqueue( object : Callback<DadosChuck> {
+        result.enqueue(object : Callback<DadosChuck> {
             override fun onResponse(call: Call<DadosChuck>?, response: Response<DadosChuck>?) {
                 if (response?.code() == 200) {
-                    var responseBody = response.body()!!
-                    var textView = textView()
-                    textView.text = responseBody.value
+                    val dadosChuck = response.body()
+                    binding.chuckNorri.text = dadosChuck?.value
                 }
             }
 
-            override fun onFailure(call: Call<DadosChuck>, t: Throwable) {
-                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+            override fun onFailure(call: Call<DadosChuck>?, t: Throwable?) {
+                Toast.makeText(this@ActivityChuckNorrisJokes, t?.message, Toast.LENGTH_SHORT).show()
             }
         })
-
-
-
     }
 }
+
