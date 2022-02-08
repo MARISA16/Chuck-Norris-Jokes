097de2da24047d8a51cf968c9c48a7504425185b
CIANDT\marisah <marisa2014antniomarisa@outlook.com>
2022-02-08 19:33:03 -0300

    Implement viewModel

--- app/src/main/java/com/marisa/chucknorrisjokes/viewmodelactivity/ViewModelActivity.kt
+++ app/src/main/java/com/marisa/chucknorrisjokes/viewmodelactivity/ViewModelActivity.kt
@@ -1,15 +1,37 @@
 package com.marisa.chucknorrisjokes.viewmodelactivity
 
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.marisa.chucknorrisjokes.models.DadosChuck
 import com.marisa.chucknorrisjokes.repositories.Repository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 //   a fun é minha  resposta
-class ViewModelActivity : ViewModel(){
+class ChuckNorrisJokesViewModel : ViewModel(){
+     private val joke= MutableLiveData<String>()
+    val _joke: LiveData<String> =joke
     val repository =Repository()
 
     fun getChuck(){
 
-        val response = repository.getChuck()
+        val result = repository.getChuck()
+
+        result.enqueue(object : Callback<DadosChuck> {
+            override fun onResponse(call: Call<DadosChuck>?, response: Response<DadosChuck>?) {
+                if (response?.code() == 200) {
+                    val dadosChuck = response.body()
+                    joke.postValue(dadosChuck?.value)
+                }
+            }
+
+            override fun onFailure(call: Call<DadosChuck>?, t: Throwable?) {
+
+            }
+        })
 
 
     }
